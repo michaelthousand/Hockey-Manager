1421099da76276fd52ebc9e77045bd855ee7e792
Michael Thousand <michael.thousand@outlook.com>
2023-03-25 00:28:34 -0500

    version 1.0.1
    quick adds to help map out next steps

--- game/models.py
+++ game/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Game(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    opponent = models.CharField(max_length=50)
+
+    # Probably will need to add two things:
+    # an event list or something like that
+    # something for time on ice
+
+    def __str__(self):
+        return f"Date: {self.date}, Opponent: {self.opponent}"
+
+
+
 class Event(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     date = models.DateField()
